267f7889460c9d6a4381ef4aef880403e891b0c8
Jens Melgaard <jens.melgaard@outlook.com>
2023-09-21 09:33:45 +0200

    Create install-desktop.sh

---
+++ install-desktop.sh
@@ -0,0 +1,6 @@
+!#/bin/bash
+#source: https://phoenixnap.com/kb/how-to-install-a-gui-on-ubuntu
+sudo apt update && sudo apt upgrade
+sudo apt install slim
+sudo apt install ubuntu-desktop
+sudo reboot
